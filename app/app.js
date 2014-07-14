ede63748eeae7a3cc68455edd0e70bdcf1b73b35
Walter <wbrand@gmail.com>
2014-07-14 10:44:00 +0200

    extended

--- app/app.js
+++ app/app.js
@@ -5,19 +5,22 @@ angular.module('app').config(function($stateProvider, $urlRouterProvider){
         name: 'home',
         url: '/',
         template: '<b>Home template :{{data.stateName}}</b>',
-        controller: 'myController'
+        controller: 'myController',
+        files :[]
     })
 
     $stateProvider.state({
         name: 'nested',
         url:'/nested',
-        template: '<div ui-view="nested.a"></div>'
+        template: '<div ui-view="nested.a"></div>',
+        files :[]
     });
 
     $stateProvider.state({
         name: 'nested.a',
         template: '<i>Nested {{data.stateName}}</i>',
-        controller : 'myController'
+        controller : 'myController',
+        files :[]
     });
 
 });
--- app/mycontroller.js
+++ app/mycontroller.js
@@ -2,5 +2,8 @@ angular.module('app').controller('myController',function($scope, $state){
     $scope.data = {
         stateName: $state.current.name
     }
-    console.log($state)
+
+    $scope.$on('$locationChangeStart', function(evt){
+        //evt.preventDefault();
+    })
 });
